57c70744ec918cb2b7635cb5ecc4b197a6f75686
doyeon <doyun20445@gmail.com>
2026-07-31 14:35:07 +0900

    chore: 초기 유저 삽입 쿼리문 추가

--- db/mysql/dml.sql
+++ db/mysql/dml.sql
@@ -2,6 +2,9 @@
 insert into member (student_id, password, name, email, department, role)
 values('12234063', '1234', 'doyeon', 'doyun20445@gmail.com', 'CS', 'USER');
 
+INSERT INTO member (student_id, password, name, email, department, role)
+VALUES (12234063, '$2b$10$1Y8VEZ2RqDs355fCwYFX9ezVnFjYcS6oLGnL1pxVq0ebCDATBigGG', 'doyeon', 'doyun20445@gmail.com', 'CS', 'USER');
+
 # 임의의 ADMIN 삽입
 insert into member (student_id, password, name, email, department, role)
 values('00000000', '1234', 'admin', 'admin@gmail.com', 'none', 'ADMIN');
